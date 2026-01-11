71d119511ce4fc92020fdd9317261f23be05055b
Kurhan Artem <kurhanartem@MacBook-Air-uzivatela-Kurhan.local>
2026-01-11 20:02:49 +0100

    Finished task

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,9 +1,14 @@
+from typing import List
+
+from app.cinema.bar import Customer
+from app.people.cinema_staff import Cleaner
+
 
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name, customers, cleaning_staff):
+    def movie_session(self, movie_name: str, customers: List[Customer], cleaning_staff: Cleaner) -> None:
         print(f"Movie session: {movie_name} started in hall {self.number}.")
 
         for customer in customers:
