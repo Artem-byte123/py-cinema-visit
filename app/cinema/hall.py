e98d46d1c51572f14db2c2d3dd88a0db3d0db244
Kurhan Artem <kurhanartem@MacBook-Air-uzivatela-Kurhan.local>
2026-01-12 15:21:32 +0100

    Finished task

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -8,7 +8,8 @@ class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customers: List[Customer], cleaning_staff: Cleaner) -> None:
+    def movie_session(self, movie_name: str, customers: List[Customer],
+                      cleaning_staff: Cleaner) -> None:
         print(f"Movie session: {movie_name} started in hall {self.number}.")
 
         for customer in customers:
--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
-from app.people.customer import Customer
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str) -> None:
     for customer in customers:
         CinemaBar.sell_product(product=customer.food, customer=customer)
     hall = CinemaHall(hall_number=hall_number)
     cleaning_staff = Cleaner(name=cleaner)
-    hall.movie_session(movie_name=movie, customers=customers, cleaning_staff=cleaning_staff)
+    hall.movie_session(movie_name=movie, customers=customers,
+                       cleaning_staff=cleaning_staff)
